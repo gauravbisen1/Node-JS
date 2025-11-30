4db3b82a38fae42649c38b8e3eb4adadfa8fc8a2
gauravbisen1 <gouravbisen123@gmail.com>
2025-11-30 17:50:20 +0530

    .

--- server.js
+++ server.js
@@ -1,16 +1,45 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
+const Person = require('./models/Person');
 let port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`);                      
 });
 
 app.get('/', (req,res)=>{
-    res.send('Hello World from Node.js server!');
+    res.send('Node.js server!');
 });
 
-app.get('/test', (req,res)=>{
-    res.send('This is a test endpoint.');
-})
+// app.post('/person', (req,res)=>{
+//     const data = req.body;
+//     const newPerson = new Person(data);
+//     newPerson.save((err,savedPerson)=>{
+//         if(err){
+//             console.log('Error saving person:', err);
+//             res.status(500).send('Error saving person');
+//         }else{
+//             console.log('Person saved:', savedPerson);
+//             res.status(201).json(savedPerson);
+//         }
+//     })
+// })
+app.post('/person', async (req, res) => {
+    try {
+      const data = req.body;
+      const newPerson = new Person(data);
+  
+      const savedPerson = await newPerson.save();   // no callback
+      console.log("Person saved:", savedPerson);
+      res.status(201).json(savedPerson);
+    } catch (err) {
+      console.error("Error saving person:", err);
+      res.status(500).send("Error saving person");
+    }
+  });
+  
+
+ 
