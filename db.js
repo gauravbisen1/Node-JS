4146eb8edc761932e3f36a11ec5ca9fdee2a750c
gauravbisen1 <gouravbisen123@gmail.com>
2025-12-01 22:51:31 +0530

    learning

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 mongoose
-  .connect("mongodb://localhost:27017/hotel")
+  .connect(process.env.MONGODB_URL)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
--- server.js
+++ server.js
@@ -3,14 +3,22 @@ const app = express();
 const db = require('./db');
 const personRoutes = require('./routes/personRoutes');
 const menuRoutes = require('./routes/menuRoutes');
+require('dotenv').config();
 
-let port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+const logRequest = (req,res,next)=>{
+  console.log(`[${new Date().toDateString()}] Request made to : ${req.originalUrl}`);
+  next();
+}
+
+app.use(logRequest);
 
 app.get('/', (req, res) => {
   res.send('Node.js server!');
